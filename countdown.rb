427fc93dbaf836ec5dbdaa12131d1a39e9f84d31
Kitty Davies <kittyfdavies@gmail.com>
2018-03-10 01:07:58 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,13 @@
 #write your code here
+
 def countdown(seconds)
   while seconds > 0
     puts "#{seconds} SECOND(S)!"
     seconds -= 1
-    sleep(1)
   end
   "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds)
+  sleep(5)
 end
